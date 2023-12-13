4231c8214a88fdc1bb473d9f4bd456433b77b211
Nathan <nathanlee3296@hotmail.co.uk>
2023-12-13 14:55:14 +0000

    sort by votes and date. Added topic search to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,68 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../Assets/Logo-transparent.png";
 import SearchOffCanvas from "./SearchOffcanvas";
-import { useState } from "react";
-import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { getArticleTopics } from "../Requests/makeRequests";
+import { Button as ButtonMUI } from "@mui/material";
+import { Celebration } from "@mui/icons-material";
 
 export default function Header() {
+	const navigate = useNavigate();
+
+	const [topics, setTopics] = useState([]);
+	const [chosenTopic, setChosenTopic] = useState("All");
+
 	const [showSearch, setShowSearch] = useState(false);
 	const handleShowSearch = () => setShowSearch(true);
+
+	useEffect(() => {
+		getArticleTopics().then(({ data: { topics } }) => {
+			setTopics(topics);
+		});
+	}, []);
+
+	useEffect(() => {
+		handleSubmission();
+	}, [chosenTopic]);
+
+	const handleSubmission = () => {
+		navigate({
+			pathname: `/search/${chosenTopic}`,
+		});
+	};
+
 	return (
 		<div id="header">
 			<img id="header-image" src={logo}></img>
 			<nav id="navbar">
 				<Link to="/">
-					<Button id="nav-link1">Front Page</Button>
+					<ButtonMUI id="nav-link1">Front Page</ButtonMUI>
 				</Link>
 				|
-				<Button id="nav-link2" onClick={handleShowSearch}>
+				<ButtonMUI id="nav-link2" onClick={handleShowSearch}>
 					Search
-				</Button>
+				</ButtonMUI>
 				<SearchOffCanvas
 					setShowSearch={setShowSearch}
 					showSearch={showSearch}
 				/>
 			</nav>
+			<Form.Select
+				id="search-topic-header"
+				onChange={(e) => {
+					setChosenTopic(e.target.value);
+				}}
+			>
+				<option>All</option>
+				{topics.map((topic) => {
+					return (
+						<option key={topic.slug} value={topic.slug}>
+							{topic.slug}
+						</option>
+					);
+				})}
+			</Form.Select>
 		</div>
 	);
 }
