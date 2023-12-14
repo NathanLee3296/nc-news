ddaa78f800e532183331e3bb656569cdb6fa9d79
Nathan <nathanlee3296@hotmail.co.uk>
2023-12-14 13:35:21 +0000

    clean up

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -53,7 +53,6 @@ export default function Article() {
 		});
 	}, []);
 
-	console.log(comments);
 
 	return (
 		<>
--- src/Components/ArticleListLoadingSkeleton.jsx
+++ src/Components/ArticleListLoadingSkeleton.jsx
@@ -2,28 +2,6 @@ import Skeleton from "@mui/material/Skeleton";
 import { Stack } from "@mui/material";
 
 export default function LoadingSkeleton({ numToRender }) {
-	const skeleton = (
-		<Stack spacing={-1}>
-			{/* For variant="text", adjust the height via font-size */}
-			<Skeleton
-				sx={{ bgcolor: "rgba(221, 205, 198, 255)" }}
-				variant="rectangular"
-				width={350}
-				height={75}
-			/>
-			<Skeleton
-				sx={{ bgcolor: "rgba(221, 205, 198, 255)", fontSize: "4rem" }}
-				variant="text"
-			/>
-
-			<Skeleton
-				sx={{ bgcolor: "rgba(221, 205, 198, 255)" }}
-				variant="rounded"
-				width={350}
-				height={150}
-			/>
-		</Stack>
-	);
 
 	return (
 		<div className="skeleton-box">
